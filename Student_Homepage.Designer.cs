43d53be7411a7b55385583e9cce8ac7b5f69da08
Viviana Popa <49440210+Mashiron27@users.noreply.github.com>
2020-11-21 00:04:24 +0200

    Add files via upload

---
+++ Student_Homepage.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace EasyTestApp
+{
+    partial class Student_Homepage
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.welcomeMessageTitleLabel = new System.Windows.Forms.Label();
+            this.whatsUpTodayLabel = new System.Windows.Forms.Label();
+            this.thingForTodayEntryLabel = new System.Windows.Forms.Label();
+            this.remindersLabel = new System.Windows.Forms.Label();
+            this.reminderEntryLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // welcomeMessageTitleLabel
+            // 
+            this.welcomeMessageTitleLabel.AutoSize = true;
+            this.welcomeMessageTitleLabel.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Bold);
+            this.welcomeMessageTitleLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(176)))), ((int)(((byte)(162)))), ((int)(((byte)(164)))));
+            this.welcomeMessageTitleLabel.Location = new System.Drawing.Point(249, 27);
+            this.welcomeMessageTitleLabel.Name = "welcomeMessageTitleLabel";
+            this.welcomeMessageTitleLabel.Size = new System.Drawing.Size(145, 17);
+            this.welcomeMessageTitleLabel.TabIndex = 0;
+            this.welcomeMessageTitleLabel.Text = "!Welcome message";
+            // 
+            // whatsUpTodayLabel
+            // 
+            this.whatsUpTodayLabel.AutoSize = true;
+            this.whatsUpTodayLabel.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Bold);
+            this.whatsUpTodayLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(176)))), ((int)(((byte)(162)))), ((int)(((byte)(164)))));
+            this.whatsUpTodayLabel.Location = new System.Drawing.Point(45, 105);
+            this.whatsUpTodayLabel.Name = "whatsUpTodayLabel";
+            this.whatsUpTodayLabel.Size = new System.Drawing.Size(141, 17);
+            this.whatsUpTodayLabel.TabIndex = 1;
+            this.whatsUpTodayLabel.Text = ":What\'s up for Today";
+            // 
+            // thingForTodayEntryLabel
+            // 
+            this.thingForTodayEntryLabel.AutoSize = true;
+            this.thingForTodayEntryLabel.Font = new System.Drawing.Font("Century Gothic", 10F);
+            this.thingForTodayEntryLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(176)))), ((int)(((byte)(162)))), ((int)(((byte)(164)))));
+            this.thingForTodayEntryLabel.Location = new System.Drawing.Point(45, 142);
+            this.thingForTodayEntryLabel.Name = "thingForTodayEntryLabel";
+            this.thingForTodayEntryLabel.Size = new System.Drawing.Size(419, 19);
+            this.thingForTodayEntryLabel.TabIndex = 2;
+            this.thingForTodayEntryLabel.Text = " s8fypf a8ery  [ [ [8[ r;d \':h fh \'[fue9ruqeu dw  ES F8ey f\'f \'fy \'f\'-";
+            // 
+            // remindersLabel
+            // 
+            this.remindersLabel.AutoSize = true;
+            this.remindersLabel.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Bold);
+            this.remindersLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(176)))), ((int)(((byte)(162)))), ((int)(((byte)(164)))));
+            this.remindersLabel.Location = new System.Drawing.Point(45, 265);
+            this.remindersLabel.Name = "remindersLabel";
+            this.remindersLabel.Size = new System.Drawing.Size(83, 17);
+            this.remindersLabel.TabIndex = 3;
+            this.remindersLabel.Text = ":Reminders";
+            // 
+            // reminderEntryLabel
+            // 
+            this.reminderEntryLabel.AutoSize = true;
+            this.reminderEntryLabel.Font = new System.Drawing.Font("Century Gothic", 10F);
+            this.reminderEntryLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(176)))), ((int)(((byte)(162)))), ((int)(((byte)(164)))));
+            this.reminderEntryLabel.Location = new System.Drawing.Point(45, 298);
+            this.reminderEntryLabel.Name = "reminderEntryLabel";
+            this.reminderEntryLabel.Size = new System.Drawing.Size(419, 19);
+            this.reminderEntryLabel.TabIndex = 4;
+            this.reminderEntryLabel.Text = " s8fypf a8ery  [ [ [8[ r;d \':h fh \'[fue9ruqeu dw  ES F8ey f\'f \'fy \'f\'-";
+            // 
+            // Homepage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(26)))), ((int)(((byte)(29)))));
+            this.Controls.Add(this.reminderEntryLabel);
+            this.Controls.Add(this.remindersLabel);
+            this.Controls.Add(this.thingForTodayEntryLabel);
+            this.Controls.Add(this.whatsUpTodayLabel);
+            this.Controls.Add(this.welcomeMessageTitleLabel);
+            this.Name = "Homepage";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Size = new System.Drawing.Size(679, 463);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label welcomeMessageTitleLabel;
+        private System.Windows.Forms.Label whatsUpTodayLabel;
+        private System.Windows.Forms.Label thingForTodayEntryLabel;
+        private System.Windows.Forms.Label remindersLabel;
+        private System.Windows.Forms.Label reminderEntryLabel;
+    }
+}
