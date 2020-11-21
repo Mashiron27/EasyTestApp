ed3a04c85c5416f8f39aded937c04f30c8003022
Interesante875 <69080395+Interesante875@users.noreply.github.com>
2020-11-21 21:42:11 +0800

    Add files via upload

---
+++ Teacher_Homepage.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EasyTestApp
+{
+    public partial class Teacher_Homepage : Form
+    {
+        public Teacher_Homepage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnMiniMax_Click(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Maximized)
+            {
+                this.WindowState = FormWindowState.Minimized;
+            }
+            else
+            {
+                this.WindowState = FormWindowState.Maximized;
+            }
+        }
+
+
+    }
+}
